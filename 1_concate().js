89668dd4026117f5f3fe43e47327c3186aded475
SaifRehman99 <saifr7493@gmail.com>
2019-07-02 17:23:55 +0500

    concate added

---
+++ 1_concate().js
@@ -0,0 +1,6 @@
+// The concat() method is used to join two or more arrays.
+
+let firstName = ['Saif', 'Abdul', 'Saim', 'Ibad'];
+let lastName = ['Rehman', 'Qadir', 'Akhtar', 'Sheikh']
+
+console.log(firstName.concat(lastName));
