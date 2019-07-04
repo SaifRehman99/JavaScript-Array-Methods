287a6957b1e03f3c0e084088761a54c066077a40
SaifRehman99 <saifr7493@gmail.com>
2019-07-04 09:18:26 +0500

    includes string updated

--- 12_includes().js
+++ 12_includes().js
@@ -2,3 +2,32 @@
 let names = ['Ahmed', 'Ali', 'Saif'];
 
 console.log(names.includes('Saif'))
+
+let isVowel = (string) => {
+
+
+    let char = ['a', 'e', 'i', 'o', 'u'];
+    var num = 0;
+
+
+    //=========this will count one vowel at a time==============//
+
+
+    // char.forEach((item) => {
+    //     if (string.includes(item)) {
+    //         num++;
+    //     }
+    // })
+
+
+    // ===============this will count every time================//
+
+    for (var i = 0; i < string.length; i++) {
+        if (string.includes(string[i])) {
+            num++;
+        }
+    }
+    return num;
+}
+
+console.log(isVowel('aaafsudasidiaowiiaoieo'))
